535ce69982767276db283e8d55044a9dfc6ccf66
dll-bytes <57524927+dll-bytes@users.noreply.github.com>
2019-11-08 18:01:15 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const TelegramBot = require('node-telegram-bot-api')
 const debug = require('./helpers')
 const TOKEN = '833678201:AAE3ypbn8w1Mt7DV7TyRcZMEKzFR28IbtMQ'
+console log ('bot has been started ..')
 
 const bot =new TelegramBot(TOKEN, {})
 //help
@@ -14,14 +15,15 @@ bot.onText(/\/help (+)/, (msg, [source, match]) => {
   const { id } =msg.chat
 
   bot.sendMessage(id, debug(msg))
-
-})
+  bot.on('massage', msg =>{
+}
  const html  =
- <strong> bug, ${msg.from.first_name}</strong>
+ <strong> bug, ${msg.from.first_name} </strong>
  <pre>
   ${debug(msg)}
   </pre>
-bot.on('massage', msg =>{
+
+
   bot.sendMessage(msg.chat.id, 'Message')
 
 })
