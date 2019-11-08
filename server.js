32f344333aaa1d491317fd1b6aaf572fd27759f7
dll-bytes <57524927+dll-bytes@users.noreply.github.com>
2019-11-08 16:11:31 +0300

    Update server.js

--- server.js
+++ server.js
@@ -10,8 +10,13 @@ bot.onText(/\/start/, msg => {
   bot.sendMessage(id, debug(msg))
 })
 
-bot.onText(/\/help (+)/, (msg, arr) => {
+bot.onText(/\/help (+)/, (msg, [source, match]) => {
   const { id } =msg.chat
 
   bot.sendMessage(id, debug(msg))
+
+})
+bot.on('massage', msg =>{
+  bot.sendMessage(msg.chat.id, 'Message')
+  
 })
