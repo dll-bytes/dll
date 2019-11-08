f6d158ec05654fe347f62e3e74d00ec2df2bc948
dll-bytes <57524927+dll-bytes@users.noreply.github.com>
2019-11-08 15:41:05 +0300

    go

--- server.js
+++ server.js
@@ -3,9 +3,15 @@ const debug = require('./helpers')
 const TOKEN = '833678201:AAE3ypbn8w1Mt7DV7TyRcZMEKzFR28IbtMQ'
 
 const bot =new TelegramBot(TOKEN, {})
-
+//help
 bot.onText(/\/start/, msg => {
-  const {id} =msg.chat
+  const { id } =msg.chat
+
+  bot.sendMessage(id, debug(msg))
+})
+
+bot.onText(/\/help (+)/, (msg, arr) => {
+  const { id } =msg.chat
 
   bot.sendMessage(id, debug(msg))
 })
